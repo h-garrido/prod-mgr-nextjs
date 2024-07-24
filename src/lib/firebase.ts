510a75ef0af6e3eb86a6baf60c4d91b0566ae5f9
h-garrido <hernanjorge.garrido@gmail.com>
2024-07-24 19:07:23 -0400

    modificaciones varias

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,14 +6,15 @@ import { doc, getDoc, getFirestore, serverTimestamp, setDoc } from "firebase/fir
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBm8vu-aYntaKrXLCdxQXEm6MwAA4QB9qg",
-  authDomain: "products-store-nextjs.firebaseapp.com",
-  projectId: "products-store-nextjs",
-  storageBucket: "products-store-nextjs.appspot.com",
-  messagingSenderId: "665825848374",
-  appId: "1:665825848374:web:7a85918be9ee27ed573c97"
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
